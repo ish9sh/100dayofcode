0d752c9310edc912ca4d742ff157a750c327d3c1
ish9sh <ish9sh@yandex.com>
2024-05-17 12:13:38 +0530

    written vector explanation function

--- 1.3 C++ STL/STL.cpp
+++ 1.3 C++ STL/STL.cpp
@@ -19,7 +19,17 @@ void exnespair()
     //accessing nested pair
     cout<<npr.second.second.first;
 }
+void exvect()
 
+{
+    vector <int> v;
+    v.emplace_back(1,2);
+    v.push_back(3);
+    // pairs in vectors 
+    vector <pair<int,int>> vr;
+    vr.emplace_back(5,6);
+    vr.push_back({7,8});
+}
 int main()
 {
     // expair();
